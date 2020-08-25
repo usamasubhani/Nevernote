2f7123a5d421cfb129d1e47010ffa48c408b829f
Usama Subhani <reachusama@protonmail.com>
2020-08-25 12:27:49 +0500

    Move app component to ./components/app/ directory

---
+++ src/components/app/App.tsx
@@ -0,0 +1,26 @@
+import React, { FC, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../reducers';
+
+const Auth = lazy(() => import('../../features/auth/Auth'));
+const Home = lazy(() => import('../../features/home/Home'));
+
+const App: FC = () => {
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Suspense fallback={<p>Loading...</p>}>
+            {isLoggedIn ? <Home /> : <Auth />}
+          </Suspense>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 import { setupServer } from './services/mirage/server';
 import { Provider } from 'react-redux';
