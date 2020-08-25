0f05dfe6b7e419de0961c683dd965ad049348578
Usama Subhani <reachusama@protonmail.com>
2020-08-25 21:07:56 +0500

    Change column spans

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -6,11 +6,11 @@ import './Home.css'
 const Home: FC = () => {
   return (
     <div className="home">
-      <div className="row">
-        <div className="col-lg-4 notebooks-container">
+      <div className="row home-container">
+        <div className="col-lg-3 notebooks-container border border-left-0 border-top-0 border-bottom-0 mt-1 off-white" >
           <Notebooks />
         </div>
-        <div className="col-lg-8 editor-container">
+        <div className="col-lg-7 editor-container">
           <Editor />
         </div>
       </div>
