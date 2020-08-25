a8f0a10069427e5d8dd886665e5da75214b26e5b
Usama Subhani <reachusama@protonmail.com>
2020-08-25 11:18:52 +0500

    Implement custom axios instance for development

---
+++ src/services/api.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import { showAlert } from '../utils';
+
+const http: AxiosInstance = axios.create({
+    baseURL: 'https://diaries.app',
+});
+
+http.defaults.headers.post['Content-Type'] = 'application/json';
+
+http.interceptors.response.use(
+    async (response: AxiosResponse): Promise<any> => {
+        if (response.status >= 200 && response.status < 300) {
+            return response.data;
+        }
+    },
+    (error: AxiosError) => {
+        const { response, request }: {
+            response?: AxiosResponse;
+            request?: XMLHttpRequest;
+        } = error;
+        if (response) {
+            if (response.status >= 400 && response.status < 500) {
+                showAlert(response.data?.data?.message, 'error');
+                return null;
+            }
+        } else if (request) {
+            showAlert('Request failed.', 'error');
+            return null;
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default http;
